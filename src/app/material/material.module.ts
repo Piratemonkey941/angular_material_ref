622b5460bce46a502ce75805e03a417fc6a25741
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-13 12:08:04 -0600

    menu

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import {MatButtonModule, } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,7 +8,7 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
 
 // import {  } from '';
 
@@ -20,6 +22,7 @@ const MaterialComponants =
   MatToolbarModule,
   MatSidenavModule ,
   FormsModule ,
+  MatMenuModule,
 
 ]
 
