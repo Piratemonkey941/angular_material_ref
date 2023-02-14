f72ab8f7434d66ee69993ad1e6d9e04bff9645f8
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-14 11:13:09 -0600

    Grids

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 // import {  } from '';
 
@@ -26,7 +27,9 @@ const MaterialComponants =
   FormsModule ,
   MatMenuModule,
   MatListModule,
-  MatDividerModule
+  MatDividerModule,
+  MatGridListModule,
+  
 ]
 
 @NgModule({
